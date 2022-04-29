cb0623813a2642032ac42ff6b8bcb87b7ed4aa26
texel <texelsensei@posteo.net>
2022-04-29 22:28:22 +0200

    feat: playaround with druid
    Super simple example to check out druid.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use druid::{AppLauncher, WindowDesc, Lens, Widget, WidgetExt, PlatformError, Data};
+use druid::widget::{Flex, Button, Label, List};
+use druid::im::Vector;
+
+// for reference: https://github.com/futurepaul/druid-todo-tutorial
+
+#[derive(Clone, Data, Lens)]
+struct Message {
+    pub text: String,
+    pub user: i32,
+}
+
+impl Message {
+    pub fn new<S: Into<String>>(text: S, user: i32) -> Self {
+        Message { text: text.into(), user }
+    }
+}
+
+#[derive(Clone, Data, Lens)]
+struct AppData {
+    pub history: Vector<Message>
+}
+
+impl AppData {
+    pub fn new() -> Self {
+        AppData {
+            history: Vector::new()
+        }
+    }
+}
+
+fn message() -> impl Widget<Message> {
+    Label::new("test")
+}
+
+fn build_ui() -> impl Widget<AppData> {
+    Flex::column()
+        .with_child(Button::new("Add").on_click(|_ctx, data: &mut AppData, _env| data.history.push_back(Message::new("test", 17))))
+        .with_child(List::new(message).lens(AppData::history))
+
+}
+
+fn main() -> Result<(), PlatformError>{
+    let state = AppData::new();
+    AppLauncher::with_window(WindowDesc::new(build_ui)).launch(state)?;
+    Ok(())
 }
