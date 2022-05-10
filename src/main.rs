4096e53990403d98d612b0cf384e01a32af36749
texel <texelsensei@posteo.net>
2022-05-10 08:59:47 +0200

    feat: play around with matrix-sdk
    Getting a feel for using the sdk.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use druid::im::Vector;
 use druid::widget::{Button, Flex, Label, List};
 use druid::{AppLauncher, Data, Lens, PlatformError, Widget, WidgetExt, WindowDesc};
 
+use matrix_sdk::reqwest::Url;
+use matrix_sdk::ruma::events::SyncMessageEvent;
+use matrix_sdk::ruma::events::room::message::MessageEventContent;
+use matrix_sdk::{self, Client, SyncSettings};
+use matrix_sdk::ruma::{user_id, events::room::message::MessageEvent};
+
 // for reference: https://github.com/futurepaul/druid-todo-tutorial
 //
 // other chat software in druid: https://github.com/loipesmas/accord
@@ -48,8 +54,28 @@ fn build_ui() -> impl Widget<AppData> {
         .with_child(List::new(message).lens(AppData::history))
 }
 
-fn main() -> Result<(), PlatformError> {
-    let state = AppData::new();
-    AppLauncher::with_window(WindowDesc::new(build_ui)).launch(state)?;
+async fn on_event(event: SyncMessageEvent<MessageEventContent>) {
+    dbg!(event);
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    /*
+    let blocking_task = tokio::task::spawn_blocking( || {
+        let state = AppData::new();
+        AppLauncher::with_window(WindowDesc::new(build_ui)).launch(state)?;
+        Ok(())
+    });
+    */
+
+    //blocking_task.await.unwrap()
+
+    let client = Client::new(Url::parse("https://matrix.org")?)?;
+
+    client.register_event_handler(on_event).await;
+    client.login("user", "pw", None, Some("at.texel.maruc")).await?;
+
+    client.sync(SyncSettings::new()).await;
+
     Ok(())
 }
